c01d85917db95b7270981d21a85ec079a89111ed
vishnuprasadbhat <bhatvishnu.1@gmail.com>
2022-02-03 01:39:06 +0530

    fixed Fraction is not defined issue-5

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fraction.js';
+import Fraction from 'fraction.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -131,7 +131,9 @@ class RecipeView extends View {
                   <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${
-                  ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                  ing.quantity
+                    ? new Fraction(ing.quantity).toFraction(true)
+                    : ''
                 }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
